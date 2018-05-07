d68ac1575a0d26400f79ad80700b8a1ba0eb5997
fralonra <fralonra@aliyun.com>
2018-05-07 15:30:49 +0800

    get info by type. random rgb and hex

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
+const data = require('./data');
+const keys = Object.keys(data);
 
-const raw = fs.readFileSync('./raw').toString();
-const data = {};
+const chromaticity = {
+  get (type) {
+    return data[type] || null;
+  },
 
-const lines = Array.from(raw.split('\n'));
-lines.forEach(l => {
-  const tmp = l.trim().split(/\s+/);
-  const type = tmp[0];
-  const r = tmp[3];
-  const g = tmp[4];
-  const b = tmp[5];
-  const hex = tmp[6];
-  data[type] = {
-    class: type,
-    r,
-    g,
-    b,
-    hex
-  };
-});
+  randomRgb () {
+    const stellar = data[keys[Math.round(Math.random() * keys.length)]];
+    return {
+      r: stellar.r,
+      g: stellar.g,
+      b: stellar.b
+    };
+  },
 
-fs.writeFileSync('./data.json', JSON.stringify(data));
+  randomHex () {
+    return data[keys[Math.round(Math.random() * keys.length)]].hex;
+  }
+};
+
+module.exports = chromaticity;
